1202ad3dcebe191eef548a52cdb373f6fd5d2cbb
CRISTHIAN <criacamu@gmail.com>
2019-10-31 22:39:51 -0500

    Mi primer commit

---
+++ app/src/main/java/com/nitrocanar/matlevel/controller/Login.java
@@ -0,0 +1,16 @@
+package com.nitrocanar.matlevel.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nitrocanar.matlevel.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
